ee412267434efa4c61cb4b3049a96f2e50507fe3
brewer-slack <bslack@zagmail.gonzaga.edu>
2018-02-01 19:40:10 -0800

    add matrix grid

--- Cipher.java
+++ Cipher.java
@@ -2,7 +2,10 @@ import java.util.ArrayList;
 
 public class Cipher {
     // fields
-    private String plainText;
+    private String pText;
+    private String matrixText;
+    private String kText;
+    private String kaText;
     private String cText;
     private String cText2;
     private String key;
@@ -10,18 +13,20 @@ public class Cipher {
     private int keyLength;
     private ArrayList<String> preSortArray;
     private ArrayList<String> postSortArray;
+    private ArrayList<String> matrixArray;
 
     // DVC
     public Cipher(){
-        this.plainText = "test code";
-        this.cText = "placeholder";
+        this.pText = "SEEMEAT10";
+        this.cText = "XAGXGXVVGXAGXVAFXGXXX";
         this.key = "ENCRYPT";
+        this.keyAlpha = "CENPRTY";
         this.keyLength = 7;
     }
 
     // EVC
     public Cipher(String plainText, String cipherText, String key) {
-        this.plainText = plainText;
+        this.pText = plainText;
         this.cText = cipherText;
         this.key = key;
         this.keyLength = key.length();
@@ -31,7 +36,7 @@ public class Cipher {
      *
      * @return
      */
-    private String Encrypt(){
+    private String encrypt(){
 
 
 
@@ -40,12 +45,60 @@ public class Cipher {
         return cText;
     }
 
+    private ArrayList<String> createMatix(){
+        this.matrixArray = new ArrayList<>();
+
+        matrixArray.add("F");
+        matrixArray.add("L");
+        matrixArray.add("1");
+        matrixArray.add("A");
+        matrixArray.add("0");
+        matrixArray.add("2");
+
+        matrixArray.add("J");
+        matrixArray.add("D");
+        matrixArray.add("W");
+        matrixArray.add("3");
+        matrixArray.add("G");
+        matrixArray.add("U");
+
+        matrixArray.add("C");
+        matrixArray.add("I");
+        matrixArray.add("Y");
+        matrixArray.add("B");
+        matrixArray.add("4");
+        matrixArray.add("P");
+
+        matrixArray.add("R");
+        matrixArray.add("5");
+        matrixArray.add("Q");
+        matrixArray.add("8");
+        matrixArray.add("V");
+        matrixArray.add("E");
+
+        matrixArray.add("6");
+        matrixArray.add("K");
+        matrixArray.add("7");
+        matrixArray.add("Z");
+        matrixArray.add("M");
+        matrixArray.add("X");
+
+        matrixArray.add("S");
+        matrixArray.add("N");
+        matrixArray.add("H");
+        matrixArray.add("0");
+        matrixArray.add("T");
+        matrixArray.add("9");
+
+        return matrixArray;
+    }
+
     public String getPlainText() {
-        return plainText;
+        return pText;
     }
 
     public void setPlainText(String plainText) {
-        this.plainText = plainText;
+        this.pText = plainText;
     }
 
     public String getcText() {
